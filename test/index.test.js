ac67e9af42b10af7fdbb4a054752c309c3372f35
Alon Nachshon <alonn@wix.com>
2017-05-11 14:59:55 +0300

    refactor some tests

--- test/index.test.js
+++ test/index.test.js
@@ -8,264 +8,180 @@ describe('Index', () => {
         driver = new Driver();
 
         driver
-            .given.css('.foo { --bar: "color(color-4)"; color: "color(--bar)";}')
-            .defaultSiteColors()
-            .styleParams({
-                numbers: {},
-                colors: {},
-                fonts: {}
-            })
-            .siteTextPresets({});
+        .given.css('.foo { --bar: "color(color-4)"; color: "color(--bar)";}')
+        .defaultSiteColors()
+        .styleParams({
+            numbers: {},
+            colors: {},
+            fonts: {}
+        })
+        .siteTextPresets({});
     });
 
-    it('should update on init', (done) => {
-        driver.when.init().then(() => {
+    it('should update on init', () => {
+        return driver.when.init().then(() => {
             expect(getOverrideStyleCallArg(driver)).to.equal('.foo { --bar: #717070; color: #717070;}');
-            done();
-        }).catch(err => {
-            setTimeout(function () {
-                throw err;
-            });
         });
     });
 
-    it('should update style on style change event', (done) => {
-        driver.when.init().then(() => {
-
-            driver.when.updateStyleParams().then(() => {
-                expect(getOverrideStyleCallArg(driver, 1)).to.equal('.foo { --bar: #717070; color: #717070;}');
-                done();
-            }).catch(err => {
-                setTimeout(function () {
-                    throw err;
-                });
-            });
-        }).catch(err => {
-            setTimeout(function () {
-                throw err;
-            });
+    it('should update style on style change event', () => {
+        return driver.when.init()
+        .then(driver.when.updateStyleParams)
+        .then(() => {
+            expect(getOverrideStyleCallArg(driver, 1)).to.equal('.foo { --bar: #717070; color: #717070;}');
         });
     });
 
-    it('should support colors from settings', (done) => {
+    it('should support colors from settings', () => {
         driver
-            .given.css('.foo {color: "color(--my_var)";}')
-            .defaultSiteColors()
-            .styleParams({
-                numbers: {},
-                colors: {
-                    'my_var': {value: 'red'}
-                },
-                fonts: {}
-            })
-            .siteTextPresets({});
+        .given.css('.foo {color: "color(--my_var)";}')
+        .defaultSiteColors()
+        .styleParams({
+            numbers: {},
+            colors: {
+                'my_var': {value: 'red'}
+            },
+            fonts: {}
+        })
+        .siteTextPresets({});
 
-        driver.when.init().then(() => {
-
-            driver.when.updateStyleParams().then(() => {
-                expect(getOverrideStyleCallArg(driver, 1)).to.equal('.foo { color: red;}');
-                done();
-            }).catch(err => {
-                setTimeout(function () {
-                    throw err;
-                });
-            });
-        }).catch(err => {
-            setTimeout(function () {
-                throw err;
-            });
+        return driver.when.init()
+        .then(driver.when.updateStyleParams)
+        .then(() => {
+            expect(getOverrideStyleCallArg(driver, 1)).to.equal('.foo { color: red;}');
         });
     });
 
-    it('should support fonts from settings', (done) => {
+    it('should support fonts from settings', () => {
         driver
-            .given.css('.foo {font: "font(--my_var)";}')
-            .defaultSiteColors()
-            .styleParams({
-                numbers: {},
-                colors: {},
-                fonts: {
-                    "my_var": {
-                        "value": "font-family:'mr de haviland','cursive';",
-                        "index": 93,
-                        "cssFontFamily": "'mr de haviland','cursive'",
-                        "family": "mr de haviland",
-                        "fontParam": true,
-                        "size": 0,
-                        "style": {
-                            "bold": false,
-                            "italic": false,
-                            "underline": false
-                        }
+        .given.css('.foo {font: "font(--my_var)";}')
+        .defaultSiteColors()
+        .styleParams({
+            numbers: {},
+            colors: {},
+            fonts: {
+                "my_var": {
+                    "value": "font-family:'mr de haviland','cursive';",
+                    "index": 93,
+                    "cssFontFamily": "'mr de haviland','cursive'",
+                    "family": "mr de haviland",
+                    "fontParam": true,
+                    "size": 0,
+                    "style": {
+                        "bold": false,
+                        "italic": false,
+                        "underline": false
                     }
                 }
-            })
-            .siteTextPresets({});
+            }
+        })
+        .siteTextPresets({});
 
-        driver.when.init().then(() => {
-
-            driver.when.updateStyleParams().then(() => {
-                expect(getOverrideStyleCallArg(driver, 1)).to.equal(`.foo { font: normal normal normal 17px/1.4em mr de haviland,cursive;}`);
-                done();
-            }).catch(err => {
-                setTimeout(function () {
-                    throw err;
-                });
-            });
-        }).catch(err => {
-            setTimeout(function () {
-                throw err;
-            });
+        return driver.when.init()
+        .then(driver.when.updateStyleParams)
+        .then(() => {
+            expect(getOverrideStyleCallArg(driver, 1)).to.equal(`.foo { font: normal normal normal 17px/1.4em mr de haviland,cursive;}`);
         });
     });
 
-    it('should support font string hack from settings', (done) => {
+    it('should support font string hack from settings', () => {
         driver
-            .given.css('.foo {width: "string(--my_var)";}')
-            .defaultSiteColors()
-            .styleParams({
-                numbers: {},
-                colors: {},
-                fonts: {
-                    "my_var": {
-                        "value": "100px",
-                        fontStyleParam: false
-                    }
+        .given.css('.foo {width: "string(--my_var)";}')
+        .defaultSiteColors()
+        .styleParams({
+            numbers: {},
+            colors: {},
+            fonts: {
+                "my_var": {
+                    "value": "100px",
+                    fontStyleParam: false
                 }
-            })
-            .siteTextPresets({});
+            }
+        })
+        .siteTextPresets({});
 
-        driver.when.init().then(() => {
-
-            driver.when.updateStyleParams().then(() => {
-                expect(getOverrideStyleCallArg(driver, 1)).to.equal(`.foo { width: 100px;}`);
-                done();
-            }).catch(err => {
-                setTimeout(function () {
-                    throw err;
-                });
-            });
-        }).catch(err => {
-            setTimeout(function () {
-                throw err;
-            });
+        return driver.when.init()
+        .then(driver.when.updateStyleParams)
+        .then(() => {
+            expect(getOverrideStyleCallArg(driver, 1)).to.equal(`.foo { width: 100px;}`);
         });
     });
 
-    it('should support string default value', (done) => {
+    it('should support string default value', () => {
         driver
-            .given.css('.foo {--my_var: "string(0px)"; width: "string(--my_var)";}')
-            .defaultSiteColors()
-            .styleParams({
-                numbers: {},
-                colors: {},
-                fonts: {}
-            })
-            .siteTextPresets({});
+        .given.css('.foo {--my_var: "string(0px)"; width: "string(--my_var)";}')
+        .defaultSiteColors()
+        .styleParams({
+            numbers: {},
+            colors: {},
+            fonts: {}
+        })
+        .siteTextPresets({});
 
-        driver.when.init().then(() => {
-
-            driver.when.updateStyleParams().then(() => {
-                expect(getOverrideStyleCallArg(driver, 1)).to.equal(`.foo { --my_var: 0px; width: 0px;}`);
-                done();
-            }).catch(err => {
-                setTimeout(function () {
-                    throw err;
-                });
-            });
-        }).catch(err => {
-            setTimeout(function () {
-                throw err;
-            });
+        return driver.when.init().then(driver.when.updateStyleParams)
+        .then(() => {
+            expect(getOverrideStyleCallArg(driver, 1)).to.equal(`.foo { --my_var: 0px; width: 0px;}`);
         });
     });
 
-    it('should support default values', (done) => {
+    it('should support default values', () => {
         driver
-            .given.css(':root{--my_var: "color(color-4)";} .foo {color: "color(--my_var)";}')
-            .defaultSiteColors()
-            .styleParams({
-                numbers: {},
-                colors: {
-                    'my_var': {value: 'rgba(128,110,66,0.6193647540983607)'}
-                },
-                fonts: {}
-            })
-            .siteTextPresets({});
+        .given.css(':root{--my_var: "color(color-4)";} .foo {color: "color(--my_var)";}')
+        .defaultSiteColors()
+        .styleParams({
+            numbers: {},
+            colors: {
+                'my_var': {value: 'rgba(128,110,66,0.6193647540983607)'}
+            },
+            fonts: {}
+        })
+        .siteTextPresets({});
 
-        driver.when.init().then(() => {
-
-            driver.when.updateStyleParams().then(() => {
-                expect(getOverrideStyleCallArg(driver, 1)).to.equal(':root{ --my_var: #717070;} .foo { color: rgba(128,110,66,0.6193647540983607);}');
-                done();
-            }).catch(err => {
-                setTimeout(function () {
-                    throw err;
-                });
-            });
-        }).catch(err => {
-            setTimeout(function () {
-                throw err;
-            });
+        return driver.when.init().then(driver.when.updateStyleParams)
+        .then(() => {
+            expect(getOverrideStyleCallArg(driver, 1)).to.equal(':root{ --my_var: #717070;} .foo { color: rgba(128,110,66,0.6193647540983607);}');
         });
     });
 
-    it('has plugin support', done => {
+    it('has plugin support', () => {
         driver.given.css('.foo {bar: "increment(number(--baz))"px; --baz: 1;}')
-            .plugin('increment', params => parseInt(params[0]) + 1);
+        .plugin('increment', params => parseInt(params[0]) + 1);
 
-        driver.when.init().then(() => {
-            expect(getOverrideStyleCallArg(driver))
-                .to.equal('.foo { bar: 2px; --baz: 1;}');
-            done();
-        }).catch(err => {
-            setTimeout(function () {
-                throw err;
-            });
+        return driver.when.init().then(() => {
+            expect(getOverrideStyleCallArg(driver)).to.equal('.foo { bar: 2px; --baz: 1;}');
         });
     });
 
-    it('has declaration plugin support', done => {
+    it('has declaration plugin support', () => {
         driver.given.css('.foo {bar: 4;}')
-            .declarationPlugin((key, val) => ({
-                key: 'ZzZ' + key + 'ZzZ',
-                value: '#' + val + '#'
-            }));
+        .declarationPlugin((key, val) => ({
+            key: 'ZzZ' + key + 'ZzZ',
+            value: '#' + val + '#'
+        }));
 
-        driver.when.init().then(() => {
-            expect(getOverrideStyleCallArg(driver))
-                .to.equal('.foo { ZzZbarZzZ: #4#;}');
-            done();
-        }).catch(err => {
-            setTimeout(function () {
-                throw err;
-            });
+        return driver.when.init().then(() => {
+            expect(getOverrideStyleCallArg(driver)).to.equal('.foo { ZzZbarZzZ: #4#;}');
         });
     });
 
-    it('should support double font reference', done => {
+    it('should support double font reference', () => {
         driver.given.css('.font-test{--some-font: "font(Body-M)"; font: "font(--some-font)";}')
-            .siteTextPresets({
-                'Body-M': {
-                    displayName: "Paragraph 2",
-                    editorKey: "font_8",
-                    fontFamily: "din-next-w01-light",
-                    lineHeight: "1.4em",
-                    size: "16px",
-                    style: "normal",
-                    value: "font:normal normal normal 16px/1.4em din-next-w01-light,din-next-w02-light,din-next-w10-light,sans-serif",
-                    weight: "normal"
-                }
-            });
+        .siteTextPresets({
+            'Body-M': {
+                displayName: "Paragraph 2",
+                editorKey: "font_8",
+                fontFamily: "din-next-w01-light",
+                lineHeight: "1.4em",
+                size: "16px",
+                style: "normal",
+                value: "font:normal normal normal 16px/1.4em din-next-w01-light,din-next-w02-light,din-next-w10-light,sans-serif",
+                weight: "normal"
+            }
+        });
 
-        driver.when.init().then(() => {
+        return driver.when.init().then(() => {
             expect(getOverrideStyleCallArg(driver))
-                .to.equal('.font-test{ --some-font: normal normal normal 16px/1.4em din-next-w01-light,din-next-w02-light,din-next-w10-light,sans-serif; font: normal normal normal 16px/1.4em din-next-w01-light,din-next-w02-light,din-next-w10-light,sans-serif;}');
-            done();
-        }).catch(err => {
-            setTimeout(function () {
-                throw err;
-            });
+            .to.equal('.font-test{ --some-font: normal normal normal 16px/1.4em din-next-w01-light,din-next-w02-light,din-next-w10-light,sans-serif; font: normal normal normal 16px/1.4em din-next-w01-light,din-next-w02-light,din-next-w10-light,sans-serif;}');
         });
     });
 
