0fb9384274905049c46d126707d3991d75739b6c
Eran Shabi <eranshabi@gmail.com>
2017-07-16 16:27:09 +0300

    e2e now waits for text to render before tests start

--- test/e2e/app.e2e.ts
+++ test/e2e/app.e2e.ts
@@ -1,37 +1,38 @@
 describe('Style Processor Scenario', () => {
-  beforeEach(async (done) => {
-    browser.executeAsyncScript((excuteDone) => {
-      window.name = 'E2E';
-      excuteDone();
-    });
-    await browser.get('/');
-    done();
-  });
-
-  it('should not change the number of style tags', async () => {
-    const styleNum = await $$('style').count();
-    browser.executeAsyncScript((excuteDone) => {
-      window.styleProcessor.init({})
-        .then(excuteDone);
+    beforeEach(async (done) => {
+        browser.executeAsyncScript((excuteDone) => {
+            window.name = 'E2E';
+            excuteDone();
+        });
+        await browser.get('/');
+        done();
     });
 
-    expect(await ($('[data-hook="text"]').getCssValue('color'))).toBe('rgba(255, 255, 255, 1)');
-    expect(await $$('style').count()).toBe(styleNum);
-  });
+    it('should not change the number of style tags', async () => {
+        await $('[data-hook="text"]');
+        const styleNum = await $$('style').count();
+        browser.executeAsyncScript((excuteDone) => {
+            window.styleProcessor.init({})
+                .then(excuteDone);
+        });
 
-  it('should update styles after change form sdk', async () => {
-    const styleNum = await $$('style').count();
-    browser.executeAsyncScript((excuteDone) => {
-      window.styleProcessor.init({})
-        .then(excuteDone);
+        expect(await ($('[data-hook="text"]').getCssValue('color'))).toBe('rgba(255, 255, 255, 1)');
+        expect(await $$('style').count()).toBe(styleNum);
     });
 
-    browser.executeAsyncScript((excuteDone) => {
-      window.changeStyles();
-      excuteDone();
-    });
+    it('should update styles after change form sdk', async () => {
+        const styleNum = await $$('style').count();
+        browser.executeAsyncScript((excuteDone) => {
+            window.styleProcessor.init({})
+                .then(excuteDone);
+        });
 
-    expect(await ($('[data-hook="text"]').getCssValue('color'))).toBe('rgba(0, 0, 0, 1)');
-    expect(await $$('style').count()).toBe(styleNum);
-  });
+        browser.executeAsyncScript((excuteDone) => {
+            window.changeStyles();
+            excuteDone();
+        });
+
+        expect(await ($('[data-hook="text"]').getCssValue('color'))).toBe('rgba(0, 0, 0, 1)');
+        expect(await $$('style').count()).toBe(styleNum);
+    });
 });
